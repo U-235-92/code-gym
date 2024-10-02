5750acfb6aa47258c9cdc87e6e6aa08fe8e3abc4
U-235-92 <a745296@yandex.ru>
2024-10-02 18:50:35 +0400

    Add some number of simple tasks

---
+++ src/main/java/aq/gym/console_char_converter/Main.java
@@ -0,0 +1,29 @@
+package aq.gym.console_char_converter;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		final char DOT_SYMBOL = '.';
+		final int SHIFT = 32;
+		final int MIN_UPPER_CASE_CHAR_CODE = 65;
+		final int MAX_UPPER_CASE_CHAR_CODE = 90;
+		final int MIN_LOWER_CASE_CHAR_CODE = 97;
+		final int MAX_LOWER_CASE_CHAR_CODE = 122;
+		boolean isRun = true;
+		while (isRun) {
+			char readChar = (char) System.in.read();
+			if (readChar == DOT_SYMBOL) {
+				isRun = false;
+			} else {
+				if(readChar >= MIN_UPPER_CASE_CHAR_CODE && readChar <= MAX_UPPER_CASE_CHAR_CODE) {
+					readChar += SHIFT;
+				} else if(readChar >= MIN_LOWER_CASE_CHAR_CODE && readChar <= MAX_LOWER_CASE_CHAR_CODE) {
+					readChar -= SHIFT;
+				}
+				System.out.print(readChar);
+			}
+		}
+	}
+}
