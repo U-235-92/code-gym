71da1c8c399623202be35ecece272309c8e1836f
Nightbringer <a745296@yandex.ru>
2024-10-04 00:49:15 +0400

    Queue

--- src/main/java/aq/gym/queue/Queue.java
+++ src/main/java/aq/gym/queue/Queue.java
@@ -3,18 +3,53 @@ package aq.gym.queue;
 public class Queue {
 
 	private int[] queue;
-	private int popIndex = 0;
-	private int pushIndex = 0;
+	private int rear;
+	private int front;
+	private int size;
 
 	public Queue(int size) {
 		queue = new int[size];
+		rear = 0;
+		front = 0;
+		size = 0;
 	}
 	
-	public void push(int val) {
-		
+	public void insert(int val) {
+		if(isFull()) {
+			System.out.println("Queue is full");
+		} else {
+			if(front == queue.length - 1) {
+				queue[front] = val;
+				front = 0;
+			} else {				
+				queue[front++] = val;
+			}
+			size++;
+			System.out.println("Add: " + val);
+		}
 	}
 
-	public void pop() {
-		
+	public void remove() {
+		if(isEmpty()) {
+			System.out.println("Queue is empty");
+		} else {
+			int val = 0;
+			if(rear == queue.length - 1) {
+				val = queue[rear];
+				rear = 0;
+			} else {
+				val = queue[rear++];
+			}
+			size--;
+			System.out.println("Get: " + val);
+		}
+	}
+	
+	private boolean isFull() {
+		return size == queue.length;
+	}
+	
+	private boolean isEmpty() {
+		return size == 0;
 	}
 }
