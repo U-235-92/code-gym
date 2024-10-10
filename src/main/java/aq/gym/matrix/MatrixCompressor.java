805afacc6e19a8b6caa833846184ac6b94fd9215
U-235-92 <a745296@yandex.ru>
2024-10-10 11:49:23 +0400

    Zero rows and columns mtrix compressor

--- src/main/java/aq/gym/matrix/MatrixCompressor.java
+++ src/main/java/aq/gym/matrix/MatrixCompressor.java
@@ -8,58 +8,67 @@ import java.util.stream.Collectors;
 public class MatrixCompressor {
 
 	public static void main(String[] args) {
-		int[][] matrix = { { 0, 2, 3, 4 }, { 0, 0, 0, 0 }, { 0, 8, 0, 9 }, { 0, 0, 0, 0 } };
+		int[][] matrix = { { 0, 2, 3, 4 }, { 0, 5, 6, 7 }, { 0, 8, 0, 9 }, { 0, 0, 0, 0 } };
 		deleteAllRowsAndColumnsContainsZero(matrix);
 	}
 	
 	private static void deleteAllRowsAndColumnsContainsZero(int[][] matrix) {
-		List<Integer> numbers = Arrays.stream(matrix).flatMap(array -> Arrays.stream(array).boxed()).collect(Collectors.toList());
-		
-		
-//		List<Integer> columnsToDelete = new ArrayList<Integer>();
-//		List<Integer> rowsToDelete = new ArrayList<Integer>();
-//		int step = matrix.length;
-//		int[] zeroRowController = new int[matrix.length];
-//		int zeroColumnController = 0;
-//		for(int i = 0; i < matrix.length; i++) {
-//			if(Arrays.compare(zeroRowController, matrix[i]) == 0) {
-//				int rowToDelete = i;
-//				rowsToDelete.add(rowToDelete);
-//			}
-//			int j = 0;
-//			for(j = 0; j < numbers.size(); j += step) {
-//				if(numbers.get(j) == 0) {
-//					zeroColumnController++;
-//				}
-//			}
-//			if(zeroColumnController == matrix.length) {				
-//				int columnToDelete = j % matrix.length;
-//				columnsToDelete.add(columnToDelete);
-//			}
-//		}
-//		int row = 0, column = 0;
-//		if(rowsToDelete.size() > 0)
-//			row = matrix.length - rowsToDelete.size();
-//		if(columnsToDelete.size() > 0)
-//			column = matrix[0].length - columnsToDelete.size();
-//		int[][] result = new int[row][column];
-//		for(int i = 0; i < matrix.length; i++) {
-//			for(int k = 0; k < rowsToDelete.size(); k++) {
-//				if(rowsToDelete.get(k) == i) {	
-//					break;
-//				} 
-//			}
-//			for(int j = 0; j < matrix[i].length; j++) {
-//				for(int m = 0; m < columnsToDelete.size(); m++) {
-//					if(columnsToDelete.get(m) == j) {
-//						continue;
-//					} else {
-//						System.out.print(matrix[i][j] + " ");
-//					}
-//				}
-//			}
-//			System.out.println();
-//		}
+		final int[] filter = new int[matrix[0].length];
+		List<Integer> zeroColumnIndexes = new ArrayList<>();
+		List<Integer> matriWithoutZeroRowsAndColumns = new ArrayList<>();
+		//filter all zero rows
+		List<Integer> matrixWithoutZeroRows = Arrays
+				.stream(matrix)
+				.filter(array -> Arrays.compare(array, filter) != 0)
+				.flatMap(array -> Arrays.stream(array).boxed())
+				.collect(Collectors.toList());
+		//get and store zero column indexes
+		for(int i = 0; i < matrix[0].length; i++) {			
+			int zeroColumnController = 0;
+			int zeroColumControllerLimit = (int) Math.sqrt(matrixWithoutZeroRows.size());
+			int step = matrix[i].length;
+			for(int j = i; j < matrixWithoutZeroRows.size(); j += step) {
+				if(matrixWithoutZeroRows.get(j) == 0) 
+					zeroColumnController++;
+				else 
+					zeroColumnController = 0;
+			}
+			if(zeroColumnController == zeroColumControllerLimit) {
+				int zeroColumnIndex = i;
+				zeroColumnIndexes.add(zeroColumnIndex);
+			}
+		}
+		//filter all zero columns
+		for(int i = 0; i < matrixWithoutZeroRows.size(); i++) {
+			for(int j = 0; j < zeroColumnIndexes.size(); j++) {
+				int zeroColumnIndex = i % matrix[0].length;
+				if(zeroColumnIndex != zeroColumnIndexes.get(j)) {
+					matriWithoutZeroRowsAndColumns.add(matrixWithoutZeroRows.get(i));
+					break;
+				}
+			}
+		}
+		//build and print matrix without zero rows and columns
+		int rows = (int) Math.sqrt(matriWithoutZeroRowsAndColumns.size());
+		int columns = (int) Math.ceil(Math.sqrt(matriWithoutZeroRowsAndColumns.size()));
+		if(columns > 0) {
+			int[] numbers = matriWithoutZeroRowsAndColumns.stream().mapToInt(number -> number.intValue()).toArray();
+			int[][] result = null;
+			if(columns >= 2) {
+				result = new int[rows][columns];
+				for(int i = 0; i < result.length; i++) {
+					System.arraycopy(numbers, i * columns, result[i], 0, columns);
+				}
+			} else {
+				result = new int[1][columns];
+				System.arraycopy(numbers, 0, result[0], 0, columns);
+			}
+			for(int[] array : result) {
+				System.out.println(Arrays.toString(array));
+			}
+		} else {
+			System.out.println("There is no result!");
+		}
 	}
 
 }
