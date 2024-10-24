2ac2cac03c82ec3c8c02e9a87b05c4e8e1870043
U-235-92 <a745296@yandex.ru>
2024-10-24 12:00:26 +0400

    Add checking of time value by pattern

---
+++ src/main/java/aq/gym/strings_strings_strings/ConsonantVowelLettersStatistic.java
@@ -0,0 +1,17 @@
+package aq.gym.strings_strings_strings;
+
+import java.util.Map;
+
+public class ConsonantVowelLettersStatistic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static Map<String, Long> getStatistic(String text) {
+		
+		Map<String, Long> statistic = null;
+		return statistic;
+	}
+}
--- src/main/java/aq/gym/strings_strings_strings/DummyTimeConverter.java
+++ src/main/java/aq/gym/strings_strings_strings/DummyTimeConverter.java
@@ -7,6 +7,9 @@ public class DummyTimeConverter {
 	}
 
 	private static String convert(String time) {
+		if(!time.matches("\\b([0][0-9]|[1][0-2]):[0-5][0-9]\\s[AP]M\\b")) {
+			throw new IllegalArgumentException();
+		}
 		int hour = Integer.valueOf(time.split(":", 2)[0]);
 		String result = "";
 		if(time.split("\\s")[1].equals("PM")) {
