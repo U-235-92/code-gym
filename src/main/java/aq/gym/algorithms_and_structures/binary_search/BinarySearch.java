71d46fbdd2a5630b5fd209601bf95c650f49651d
U-235-92 <a745296@yandex.ru>
2024-10-29 17:34:00 +0400

    Recursive binary search

--- src/main/java/aq/gym/algorithms_and_structures/binary_search/BinarySearch.java
+++ src/main/java/aq/gym/algorithms_and_structures/binary_search/BinarySearch.java
@@ -6,11 +6,13 @@ public class BinarySearch {
 
 	public static void main(String[] args) {
 		int[] numbers = {1, 2, 3, 4, 5};
-		int pos = search(numbers, 3);
-		System.out.println(pos);
+		int posRegular = searchRegularWay(numbers, 3);
+		System.out.println("Regular way: " + posRegular);
+		int posRecursive = searchRecursiveWay(numbers, 3, 0, numbers.length - 1);
+		System.out.println("Recursive way: " + posRecursive);
 	}
 
-	private static int search(int[] numbers, int search) {
+	private static int searchRegularWay(int[] numbers, int search) {
 		int left = 0, right = numbers.length - 1;
 		while(left <= right) {
 			int mid = (left + right) / 2;
@@ -23,5 +25,20 @@ public class BinarySearch {
 				left = mid + 1;
 		}
 		throw new NoSuchElementException(search + " not found");
+	}
+	
+	private static int searchRecursiveWay(int[] numbers, int search, int left, int right) {
+		int mid = (left + right) / 2;
+		int guess = numbers[mid];
+		if(left <= right) {			
+			if(guess == search) 
+				return mid;
+			else if(guess > search)
+				return searchRecursiveWay(numbers, search, left, mid - 1);
+			else
+				return searchRecursiveWay(numbers, search, mid + 1, right);
+		} 
+		throw new NoSuchElementException(search + " not found");
+		
 	}
 }
