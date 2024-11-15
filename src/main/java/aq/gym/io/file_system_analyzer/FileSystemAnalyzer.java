e36c317dfd5532e76c1cf35a5d8386f3dc1e4527
U-235-92 <a745296@yandex.ru>
2024-11-15 11:55:42 +0400

    File system analyzer. Beginning

---
+++ src/main/java/aq/gym/io/file_system_analyzer/FileSystemAnalyzer.java
@@ -0,0 +1,8 @@
+package aq.gym.io.file_system_analyzer;
+
+public class FileSystemAnalyzer {
+
+	private FileSystemAnalyzer() {}
+	
+	
+}
---
+++ src/main/java/aq/gym/io/file_system_analyzer/Main.java
@@ -0,0 +1,10 @@
+package aq.gym.io.file_system_analyzer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
