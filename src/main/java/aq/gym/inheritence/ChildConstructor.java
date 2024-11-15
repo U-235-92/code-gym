ebecd0a71d5d202800495cfe44c34db29d98e3a4
U-235-92 <a745296@yandex.ru>
2024-11-15 12:06:47 +0400

    Inheritence and no args constructor from super class

---
+++ src/main/java/aq/gym/inheritence/ChildConstructor.java
@@ -0,0 +1,8 @@
+package aq.gym.inheritence;
+
+public class ChildConstructor extends ParentConstructor {
+
+	//Won't compile if parent class hasn't got [no args] constructor
+	//By default if you don't call super() then it will call [no args] constructor from super class 
+	public ChildConstructor(String val) {} 
+}
---
+++ src/main/java/aq/gym/inheritence/ParentConstructor.java
@@ -0,0 +1,8 @@
+package aq.gym.inheritence;
+
+public class ParentConstructor {
+
+	public ParentConstructor() {}
+	
+	public ParentConstructor(int val) {}
+}
