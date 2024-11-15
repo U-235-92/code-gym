990aeec6673a29c94e0ec4807d70781b4a9cdb1b
U-235-92 <a745296@yandex.ru>
2024-11-15 16:49:48 +0400

    File system analyzer

--- src/main/java/aq/gym/io/file_system_analyzer/FileSystemAnalyzer.java
+++ src/main/java/aq/gym/io/file_system_analyzer/FileSystemAnalyzer.java
@@ -1,40 +1,120 @@
 package aq.gym.io.file_system_analyzer;
 
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileSystemAnalyzer {
 
 	private FileSystemAnalyzer() {}
 	
-	public String parent(String path) {
-		return null;
+	public static String parent(String path) {
+		String result = Paths.get(path).getParent().toString();
+		return result;
 	}
 	
 	public static String absolutePath(String file) {
-		return null;
+		String result = Paths.get(file).toAbsolutePath().toString();
+		return result;
 	}
 	
 	public static int nestedCatalogsNumber(String path) {
-		return -1;
+		int[] nestedCatalogNumberWrap = new int[] {-1};
+		try {
+			Files.walkFileTree(Paths.get(path), new SimpleFileVisitor<Path>() {
+				@Override
+				public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+					nestedCatalogNumberWrap[0]++;
+					return FileVisitResult.CONTINUE;
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return nestedCatalogNumberWrap[0];
 	}
 	
 	public static int nestedFilesNumber(String path) {
-		return -1;
+		int[] nestedFileNumberWrap = new int[] {0};
+		try {
+			Files.walkFileTree(Paths.get(path), new SimpleFileVisitor<Path>() {
+				
+				@Override
+				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+					nestedFileNumberWrap[0]++;
+					return FileVisitResult.CONTINUE;
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return nestedFileNumberWrap[0];
 	}
 	
 	public static long size(String path) {
+		try {
+			long[] sizeWrap = new long[] {0L};
+			Files.walkFileTree(Paths.get(path), new SimpleFileVisitor<Path>() {
+				@Override
+				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+					sizeWrap[0] += Files.size(file);
+					return FileVisitResult.CONTINUE;
+				}
+			});
+			return sizeWrap[0];
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return -1L;
 	}
 	
-	public List<String> findByMask(String startPath, String mask) {
-		return null;
+	public static List<String> findByMask(String startPath, String mask) {
+		List<String> result = new ArrayList<String>();
+		try {
+			startPath = absolutePath(startPath);
+			result = Files.walk(Paths.get(startPath))
+				.filter(path -> path.toString().contains(mask))
+				.map(Path::toString)
+				.collect(Collectors.toList());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 	
-	public boolean transfer(String fromPath, String toPath) {
-		return false;
+	public static void transfer(String fromPath, String toPath) {
+		try {
+			Files.move(Paths.get(fromPath), Paths.get(toPath), StandardCopyOption.ATOMIC_MOVE);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public boolean delete(String path, String... flags) {
-		return false;
+	public static void delete(String path) {
+		try {
+			Files.walkFileTree(Paths.get(path), new SimpleFileVisitor<Path>() {
+				@Override
+				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+					Files.delete(file);
+					return FileVisitResult.CONTINUE;
+				}
+				
+				@Override
+				public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+					Files.delete(dir);
+					return FileVisitResult.CONTINUE;
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/aq/gym/io/file_system_analyzer/Main.java
+++ src/main/java/aq/gym/io/file_system_analyzer/Main.java
@@ -1,10 +1,27 @@
 package aq.gym.io.file_system_analyzer;
 
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		if(args.length > 0) {
+			String fileToAnalize = "src/main/java/aq/gym/io/file_system_analyzer";
+			System.out.println("Source path: " + fileToAnalize);
+			System.out.println("Absolute path: " + FileSystemAnalyzer.absolutePath(fileToAnalize)); 
+			System.out.println("The files which contain .class: ");
+			List<String> patternFiles = FileSystemAnalyzer.findByMask(fileToAnalize, ".class");
+			if(patternFiles.size() > 0)
+				patternFiles.forEach(path -> System.out.println(path));
+			else
+				System.out.println("No files");
+			System.out.println("Number of nested catalogs: " + FileSystemAnalyzer.nestedCatalogsNumber(fileToAnalize));
+			System.out.println("Number of nested files: " + FileSystemAnalyzer.nestedFilesNumber(fileToAnalize));
+			System.out.println("Parent direcory: " + FileSystemAnalyzer.parent(fileToAnalize));
+			System.out.println("Size of path: " + FileSystemAnalyzer.size(fileToAnalize) + " bytes");
+		} else {
+			System.out.println("Specify a path");
+		}
 	}
 
 }
