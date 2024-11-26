689d3584a3fac92d528d75e1f48d0698b7bda842
U-235-92 <a745296@yandex.ru>
2024-11-26 17:41:28 +0400

    Understanding how transaction works

--- src/main/java/aq/gym/db/orm_sport_nutrition_shop/Main.java
+++ src/main/java/aq/gym/db/orm_sport_nutrition_shop/Main.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
 public class Main {
@@ -15,6 +14,32 @@ public class Main {
 		ClientDAO clientDAO = new ClientDAO(entityManager);
 		List<Client> insertedClients = List.of(new Client("Alice"), new Client("Bob"), new Client("Sarah"));
 		clientDAO.createClients(insertedClients);
+		
+//		Will persist to db after commit transaction
+//		EntityTransaction transaction = entityManager.getTransaction();
+//		transaction.begin();
+//		Client john = new Client("John");
+//		entityManager.persist(john);
+//		try {
+//			System.out.println("Press any key...");
+//			System.in.read();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		transaction.commit();
+		
+//		Will save all changes after commit transaction :-/
+//		Client bob = clientDAO.readClient(2);
+//		EntityTransaction transaction = entityManager.getTransaction();
+//		transaction.begin();
+//		bob.setName("New Bob");
+//		try {
+//			System.out.println("Press any key...");
+//			System.in.read();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		transaction.commit();
 	}
 
 }
