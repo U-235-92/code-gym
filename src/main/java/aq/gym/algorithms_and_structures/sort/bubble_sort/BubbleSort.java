73e0a03759eb529ac845add9333fb09ddd01deff
U-235-92 <a745296@yandex.ru>
2025-02-18 14:20:44 +0400

    Better version of bubble sort

--- src/main/java/aq/gym/algorithms_and_structures/sort/bubble_sort/BubbleSort.java
+++ src/main/java/aq/gym/algorithms_and_structures/sort/bubble_sort/BubbleSort.java
@@ -6,8 +6,11 @@ public class BubbleSort {
 
 	public static void main(String[] args) {
 		int[] ints = {1, 5, 2, 0, -1, 10};
+		int[] intsCopy = Arrays.copyOf(ints, ints.length); 
 		sort(ints);
+		sort2(intsCopy);
 		System.out.println(Arrays.toString(ints));
+		System.out.println(Arrays.toString(intsCopy));
 	}
 	
 	private static void sort(int[] ints) {
@@ -20,5 +23,26 @@ public class BubbleSort {
 				}
 			}
 		}
+	}
+	
+	private static void sort2(int[] ints) {
+		for(int i = 0; i < ints.length; i++) {
+			boolean wasReplace = false;
+			for(int j = 0; j < (ints.length - 1) - i; j++) {
+				if(ints[j] > ints[j + 1]) {
+					swap(j, ints);
+					wasReplace = true;
+				}
+			}
+			if(wasReplace == false) {				
+				return;
+			}
+		}
+	}
+	
+	private static void swap(int index, int[] ints) {
+		int tmp = ints[index];
+		ints[index] = ints[index + 1];
+		ints[index + 1] = tmp;
 	}
 }
