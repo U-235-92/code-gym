759af0c28189f3a920bb260489a2e0e2ac57ac86
U-235-92 <a745296@yandex.ru>
2025-03-12 10:34:59 +0400

    Algorithms studying. Added examples of using reflection. Added example of using greedy algorithm

---
+++ src/main/java/aq/gym/contests/algorithms/PermutationNumber.java
@@ -0,0 +1,26 @@
+package aq.gym.contests.algorithms;
+
+import java.util.Scanner;
+
+public class PermutationNumber {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		scanner.close();
+		if(number < 1 || number > 7) {
+			throw new IllegalArgumentException();
+		}
+		int permutationNumber = permutationNumber(number);
+		System.out.println(permutationNumber);
+	}
+	
+	private static int permutationNumber(int number) {
+		if(number == 1) {
+			return 1;
+		}
+		int result = number * permutationNumber(number - 1);
+		return result;
+	}
+
+}
