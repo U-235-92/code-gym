971ff218da059370376eaf27938f39d08f3c0a50
U-235-92 <a745296@yandex.ru>
2025-03-18 11:40:08 +0400

    Hanoi towers - experiments with 4 pegs

--- src/main/java/aq/gym/contests/algorithms/HanoiTowers.java
+++ src/main/java/aq/gym/contests/algorithms/HanoiTowers.java
@@ -11,20 +11,42 @@ public class HanoiTowers {
 		int numberOfDisc = scanner.nextInt();
 		scanner.close();
 		List<String> steps = new ArrayList<>();
-		hanoiTowers(numberOfDisc, 1, 3, steps);
+		hanoiTowers4(numberOfDisc, 1, 4, steps);
 		System.out.println(steps.size());
-		steps.stream().forEach(System.out::println);
+//		steps.stream().forEach(System.out::println);
 	}
 
 	@SuppressWarnings("unused")
-	private static void hanoiTowers(int n, int fromPeg, int toPeg, List<String> steps) {
+	private static void hanoiTowers3(int n, int fromPeg, int toPeg, List<String> steps) {
 		if(n == 1) {
 			steps.add(fromPeg + " " + toPeg);
 			return;
 		}
 		int unusedPeg = 6 - fromPeg - toPeg;
-		hanoiTowers(n - 1, fromPeg, unusedPeg, steps);
+		hanoiTowers3(n - 1, fromPeg, unusedPeg, steps);
 		steps.add(fromPeg + " " + toPeg);
-		hanoiTowers(n - 1, unusedPeg, toPeg, steps);
+		hanoiTowers3(n - 1, unusedPeg, toPeg, steps);
+	}
+	
+	@SuppressWarnings("unused")
+	private static void hanoiTowers4(int n, int fromPeg, int toPeg, List<String> steps) {
+		int unusedPeg1 = 7 - fromPeg - toPeg;
+		int unusedPeg2 = 10 - fromPeg - toPeg - unusedPeg1;
+		int k = n - 2;
+		if(n == 1) {
+			steps.add(fromPeg + " " + toPeg);
+			return;
+		}
+		if(n == 2) {
+			steps.add(fromPeg + " " + unusedPeg1);
+			steps.add(fromPeg + " " + toPeg);
+			steps.add(unusedPeg1 + " " + toPeg);
+			return;
+		}
+		hanoiTowers4(n - 2, fromPeg, unusedPeg1, steps);
+		steps.add(fromPeg + " " + unusedPeg1);
+		steps.add(unusedPeg1 + " " + toPeg);
+		steps.add(fromPeg + " " + toPeg);
+		hanoiTowers4(n - 2, unusedPeg1, toPeg, steps);
 	}
 }
