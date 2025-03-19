c48e0f45d57b08372362f4f9bb2dc7debc588873
U-235-92 <a745296@yandex.ru>
2025-03-19 23:53:20 +0400

    Max multiplication. Added O(1.5N) algorithm

--- src/main/java/aq/gym/contests/algorithms/MaxMultiplication.java
+++ src/main/java/aq/gym/contests/algorithms/MaxMultiplication.java
@@ -11,6 +11,45 @@ import java.util.stream.LongStream;
 public class MaxMultiplication {
 
 	public static void main(String[] args) {
+		stressTest();
+	}
+	
+	private static long n1_5Multiplication(long[] numbers) {
+		int idxFirstMax = 0;
+		long firstMax = Long.MIN_VALUE;
+		for(int i = 0; i < numbers.length; i++) {
+			if(numbers[i] > firstMax) {
+				firstMax = numbers[i];
+				idxFirstMax = i;
+			}
+		}
+		int mid = numbers.length / 2;
+		swap(numbers, firstMax, idxFirstMax, mid);
+		int left = 0, right = numbers.length - 1;
+		long secondMax = Long.MIN_VALUE;
+		while(left <= right) {
+			long a = numbers[left];
+			long b = numbers[right];
+			if(a > secondMax && left != mid) {
+				secondMax = a;
+			}
+			if(b > secondMax && right != mid) {
+				secondMax = b;
+			}
+			left++; right--;
+		}
+		long result = firstMax * secondMax;
+		return result;
+	}
+	
+	private static void swap(long[] numbers, long value, int from, int to) {
+		long tmp = numbers[to];
+		numbers[to] = value;
+		numbers[from] = tmp;
+	}
+	
+	@SuppressWarnings("unused")
+	private static void app() {
 		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 			long n = Long.valueOf(br.readLine());
 			long[] numbers = Arrays.stream(br.readLine().split("\\s")).mapToLong(Long::valueOf).toArray();
@@ -62,6 +101,12 @@ public class MaxMultiplication {
 		System.out.println(result);
 	}
 	
+	private static void staticN1_5Test() {
+		long[] numbers = new long[] {4, 4, 7, 8, 0, 2};
+		long result = n1_5Multiplication(numbers);
+		System.out.println(result);
+	}
+	
 	@SuppressWarnings("unused")
 	private static void n2TimeTest() {
 		int arrayLength = 10000;
@@ -76,17 +121,17 @@ public class MaxMultiplication {
 	
 	@SuppressWarnings("unused")
 	private static void stressTest() {
-		int arrayLength = 10000;
+		int arrayLength = 2;
 		int numberLimit = 2 * 100000;
 		long okCount = 1;
 		while(true) {
 			long[] numbers = LongStream.generate(() -> (long) (Math.random() * numberLimit)).limit(arrayLength).toArray();
 			long nPow2naiveResult = naiveMultiplicationNPow2(numbers);
-			long n2Result = n2Multiplication(numbers);
-			if(nPow2naiveResult == n2Result) {
+			long n1_5Result = n1_5Multiplication(numbers);
+			if(nPow2naiveResult == n1_5Result) {
 				System.out.println("OK " + (okCount++));
 			} else {
-				System.out.println("W/A: naive result: " + nPow2naiveResult + ", n2 result: " + n2Result);
+				System.out.println("W/A: naive result: " + nPow2naiveResult + ", n1_5 result: " + n1_5Result);
 				System.out.println("Input array data: " + Arrays.toString(numbers));
 				return;
 			}
