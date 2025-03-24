24e5793651977319486f0d3c0fe2ea2aa8e00998
U-235-92 <a745296@yandex.ru>
2025-03-24 17:27:40 +0400

    Billboard advertisement

--- src/main/java/aq/gym/contests/algorithms/BillboardAdvertisement.java
+++ src/main/java/aq/gym/contests/algorithms/BillboardAdvertisement.java
@@ -7,17 +7,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class BillboardAdvertisement {
 
 	public static void main(String[] args) {
-		stressTest();
+		getProfit();
 	}
 
 	private static void getProfit() {
 		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-			List<PayOffer> offers = new ArrayList<>();
+			List<PayOffer> offers = new LinkedList<>();
 			List<Integer> billboardWeeks = new ArrayList<>();
 			int[] data = Arrays.stream(br.readLine().split("\\s")).mapToInt(Integer::valueOf).toArray();
 			int billboardsNumber = data[0];
@@ -31,7 +32,7 @@ public class BillboardAdvertisement {
 				PayOffer offer = new PayOffer(data[0], data[1]);
 				offers.add(offer);
 			}
-			Collections.sort(offers, Comparator.comparing(PayOffer::getOffer).reversed());
+			Collections.sort(offers, Comparator.comparing(PayOffer::getCostPerWeek).reversed());
 			long maximalProfit = getMaximalProfitGreedy(offers, billboardWeeks);
 			System.out.println(maximalProfit);
 		} catch(IOException e) {
@@ -41,110 +42,20 @@ public class BillboardAdvertisement {
 	
 	private static long getMaximalProfitGreedy(List<PayOffer> offers, List<Integer> billboardWeeks) {
 		long maximalProfit = 0;
-		while(offers.size() > 0 && billboardWeeks.size() > 0) {
-			int maxProfitOfferIdx = 0, billboardWeeksIdx = 0;
-			long bestOfferPrice = offers.get(maxProfitOfferIdx).getOffer();
-			int offerNumberOfWeeks = offers.get(maxProfitOfferIdx).getNumberOfWeeks();
-			int billboardFreeWeeks = billboardWeeks.get(billboardWeeksIdx);
-			if(billboardFreeWeeks >= offerNumberOfWeeks) {				
-				maximalProfit += bestOfferPrice;
-				billboardFreeWeeks = billboardFreeWeeks - offerNumberOfWeeks;
-				if(billboardFreeWeeks > 0) {
-					billboardWeeks.add(billboardFreeWeeks);
-				} 
-				billboardWeeks.remove(billboardWeeksIdx);
+		int slotsRemain = billboardWeeks.size() * billboardWeeks.get(0);
+		for(PayOffer offer : offers) {
+			int bestOfferCost = offer.getCostPerWeek();
+			int bestOfferNumberWeeks = offer.getNumberOfWeeks();
+			int weekToUse = Integer.min(slotsRemain, bestOfferNumberWeeks);
+			maximalProfit += (bestOfferCost * weekToUse);
+			slotsRemain -= bestOfferNumberWeeks;
+			if(slotsRemain <= 0) {
+				break;
 			}
-			offers.remove(maxProfitOfferIdx);
 		}
 		return maximalProfit;
 	}
-	
-	private static long getMaximalProfitNaive(List<PayOffer> offers, List<Integer> billboardWeeks) {
-		long profit = Long.MIN_VALUE;
-		int totalWeeks = billboardWeeks.stream().reduce(0, Integer::sum);
-		for(int i = 0; i < offers.size(); i++) {
-			PayOffer iOffer = offers.get(i);
-			int weeks = iOffer.getNumberOfWeeks();
-			long currentProfit = iOffer.getOffer();
-			for(int j = i; j < offers.size(); j++) {
-				PayOffer jOffer = offers.get(j);
-				if(iOffer != jOffer && weeks + jOffer.getNumberOfWeeks() <= totalWeeks) {
-					weeks += jOffer.getNumberOfWeeks();
-					currentProfit += jOffer.getOffer();
-				}
-			}
-			if(currentProfit > profit) {
-				profit = currentProfit;
-			}
-		}
-		return profit;
-	}
-	
-	private static void naiveTest() {
-		int n = 2; //1 <= n <= 10^3
-		int k = 4; //1 <= k <= 10^5
-		int w = 4; //1 <= w <= 10^2
-		List<PayOffer> offers = new ArrayList<>(List.of(
-				PayOffer.of(88, 3), 
-				PayOffer.of(72, 3), 
-				PayOffer.of(32, 2),
-				PayOffer.of(93, 2)));
-		List<Integer> weeks = new ArrayList<>(List.of(4, 4));
-		long naiveProfit = getMaximalProfitNaive(offers, weeks);
-		System.out.println(naiveProfit);
-	}
-	
-	private static void greedyTest() {
-		int n = 2; //1 <= n <= 10^3
-		int k = 4; //1 <= k <= 10^5
-		int w = 4; //1 <= w <= 10^2
-		List<PayOffer> offers = new ArrayList<>(List.of(
-				PayOffer.of(88, 3), 
-				PayOffer.of(72, 3), 
-				PayOffer.of(32, 2),
-				PayOffer.of(93, 2)));
-		List<Integer> weeks = new ArrayList<>(List.of(4, 4));
-		Collections.sort(offers, Comparator.comparing(PayOffer::getWeekCost).reversed());
-		long greedyProfit = getMaximalProfitGreedy(offers, weeks);
-		System.out.println(greedyProfit);
-	}
-	
-	private static void stressTest() {
-		while(true) {
-			List<PayOffer> offers = new ArrayList<>();
-			List<Integer> weeks = new ArrayList<>();
-			List<PayOffer> offersCopy = new ArrayList<>();
-			List<Integer> weeksCopy = new ArrayList<>();
-			int n = (int) ((Math.random() * (5 - 1)) + 1); //1 <= n <= 10^3
-			int k = (int) ((Math.random() * (5 - 1)) + 1); //1 <= k <= 10^5
-			int w = (int) ((Math.random() * (5 - 1)) + 1); //1 <= w <= 10^2
-			for(int i = 0; i < n; i++) {
-				weeks.add(w);
-				weeksCopy.add(w);
-			}
-			for(int i = 0; i < k; i++) {
-				int cost = (int) ((Math.random() * 100) + 1); //1 <= cost <= 10^2
-				int week = (int) ((Math.random() * (w - 1)) + 1); //1 <= week <= w
-				PayOffer offer = new PayOffer(cost, week);
-				offers.add(offer);
-				offersCopy.add(offer);
-			}
-			Collections.sort(offersCopy, Comparator.comparing(PayOffer::getWeekCost).reversed());
-			long naiveProfit = getMaximalProfitNaive(offers, weeks);
-			long greedyProfit = getMaximalProfitGreedy(offersCopy, weeksCopy);
-			if(naiveProfit == greedyProfit) {
-				System.out.println("OK");
-			} else {
-				System.out.println("W/A");
-				System.out.println("Naive: " + naiveProfit + " Greedy: " + greedyProfit);
-				System.out.println("n = " + n + ", k = " + k + ", w = " + w);
-				System.out.println("Offers: " + offers);
-				System.out.println("Weeks: " + weeks);
-				return;
-			}
-		}
-	}
-	
+
 	private static class PayOffer {
 		
 		private int costPerWeek;
@@ -156,24 +67,12 @@ public class BillboardAdvertisement {
 			this.numberOfWeeks = numberOfWeeks;
 		}
 
-		private static PayOffer of(int costPerWeek, int numberOfWeeks) {
-			return new PayOffer(costPerWeek, numberOfWeeks);
-		}
-		
 		private int getCostPerWeek() {
 			return costPerWeek;
 		}
 		
 		private int getNumberOfWeeks() {
 			return numberOfWeeks;
-		}
-
-		private double getWeekCost() {
-			return (double) (costPerWeek / numberOfWeeks);
-		}
-		
-		private long getOffer() {
-			return costPerWeek * numberOfWeeks;
 		}
 		
 		@Override
