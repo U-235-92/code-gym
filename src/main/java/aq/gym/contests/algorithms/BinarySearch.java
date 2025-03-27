2fa5292b46455fb3394d32c452c18b72c7f79a76
U-235-92 <a745296@yandex.ru>
2025-03-27 19:02:38 +0400

    Added multi binary search

--- src/main/java/aq/gym/contests/algorithms/BinarySearch.java
+++ src/main/java/aq/gym/contests/algorithms/BinarySearch.java
@@ -8,19 +8,45 @@ import java.util.Arrays;
 public class BinarySearch {
 
 	public static void main(String[] args) {
+		multiBinarySearch();
+	}
+
+	@SuppressWarnings("unused")
+	private static void binarySearch() {
 		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 			@SuppressWarnings("unused")
 			long n = Long.valueOf(br.readLine());
 			long[] numbers = Arrays.stream(br.readLine().split("\\s")).mapToLong(Long::valueOf).toArray();
 			long q = Long.valueOf(br.readLine());
-			int idx = binarySearch(0, numbers.length - 1, numbers, q);
+			int idx = getElementIndex(0, numbers.length - 1, numbers, q);
 			System.out.println(idx);
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
 	}
-
-	private static int binarySearch(int left, int right, long[] numbers, long q) {
+	
+	@SuppressWarnings("unused")
+	private static void multiBinarySearch() {
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			long n = Long.valueOf(br.readLine());
+			long[] sorted = Arrays.stream(br.readLine().split("\\s")).mapToLong(Long::valueOf).toArray();
+			long q = Long.valueOf(br.readLine());
+			long[] noSorted = Arrays.stream(br.readLine().split("\\s")).mapToLong(Long::valueOf).toArray();
+			multiBinarySearch(sorted, noSorted);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void multiBinarySearch(long[] sort, long[] noSort) {
+		for(int i = 0; i < noSort.length; i++) {
+			int left = 0, right = sort.length - 1;
+			int idx = getElementIndex(left, right, sort, noSort[i]);
+			System.out.println(idx);
+		}
+	}
+	
+	private static int getElementIndex(int left, int right, long[] numbers, long q) {
 		while(left <= right) {			
 			int mid = (left + right) / 2;
 			if(numbers[mid] == q) {
