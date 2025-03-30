d2c8e86894ae9798704e17d3ebe4968879d4b595
U-235-92 <a745296@yandex.ru>
2025-03-30 14:14:11 +0400

    Distinct elements

---
+++ src/main/java/aq/gym/contests/easy/DistinctElements.java
@@ -0,0 +1,26 @@
+package aq.gym.contests.easy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DistinctElements {
+
+	public static void main(String[] args) {
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			int n = Integer.valueOf(br.readLine());
+			int[] numbers = Arrays.stream(br.readLine().split("\\s")).mapToInt(Integer::valueOf).toArray();
+			Map<Integer, Integer> frequencyAppearNumberMap = new HashMap<>();
+			for(int num : numbers) {
+				frequencyAppearNumberMap.computeIfAbsent(num, v -> 0);
+				frequencyAppearNumberMap.computeIfPresent(num, (k, v) -> v + 1);
+			}
+			System.out.println(frequencyAppearNumberMap.values().stream().filter(freq -> freq == 1).reduce(0, Integer::sum));
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
