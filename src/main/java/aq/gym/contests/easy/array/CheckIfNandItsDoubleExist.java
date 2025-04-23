ad96c90b77e6b6618d9d2cca764dab0de0a934b1
U-235-92 <a745296@yandex.ru>
2025-04-23 12:03:46 +0400

    Check if N and its double N (N * 2) exist

---
+++ src/main/java/aq/gym/contests/easy/array/CheckIfNandItsDoubleExist.java
@@ -0,0 +1,33 @@
+package aq.gym.contests.easy.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CheckIfNandItsDoubleExist {
+
+	public static void main(String[] args) {
+		int[] arr = {6,3};
+		boolean isExist = new CheckIfNandItsDoubleExist().checkIfExist(arr);
+		System.out.println(isExist);
+	}
+
+	public boolean checkIfExist(int[] arr) {
+		if(arr.length < 2) {
+			return false;
+		}
+        Map<Integer, Boolean> checkMap = new HashMap<>();
+        checkMap.put(arr[0], false);
+        for(int i = 1; i < arr.length; i++) {
+        	if(checkMap.containsKey(2 * arr[i])) {
+        		return true;
+        	}
+        	if(arr[i] % 2 == 0) {
+        		if(checkMap.containsKey(arr[i] / 2)) {
+            		return true;
+            	}
+        	}
+        	checkMap.put(arr[i], false);
+        }
+        return false;
+    }
+}
