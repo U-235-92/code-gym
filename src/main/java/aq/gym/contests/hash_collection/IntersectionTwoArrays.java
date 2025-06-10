f455a8a8e083c13317d3f372c893159636da84a0
U-235-92 <a745296@yandex.ru>
2025-06-10 11:42:06 +0400

    Intersection

---
+++ src/main/java/aq/gym/contests/hash_collection/IntersectionTwoArrays.java
@@ -0,0 +1,20 @@
+package aq.gym.contests.hash_collection;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class IntersectionTwoArrays {
+
+	public static void main(String[] args) {
+		int[] nums1 = {1,2,2,1}, nums2 = {2,2};
+		System.out.println(Arrays.toString(new IntersectionTwoArrays().intersection(nums1, nums2)));
+	}
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> aSet = new HashSet<>(Arrays.stream(nums1).boxed().toList());
+        Set<Integer> bSet = new HashSet<>(Arrays.stream(nums2).boxed().toList());
+        aSet.retainAll(bSet);
+        return aSet.stream().mapToInt(Integer::valueOf).toArray();
+    }
+}
