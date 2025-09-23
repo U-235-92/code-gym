84c4d396f08bcac607a6ca83b5a58ef60b7025a2
U-235-92 <a745296@yandex.ru>
2025-09-23 17:45:28 +0400

    Map modification example

--- src/main/java/aq/gym/collections/collections_things/TryToModify.java
+++ src/main/java/aq/gym/collections/collections_things/TryToModify.java
@@ -8,10 +8,28 @@ import java.util.Map;
 public class TryToModify {
 
 	public static void main(String[] args) {
+		youCanModifyKeyValuePairOfMapButItHasSomeImportantDetails();
 		youCantSortMapByEntrySet();
 		youCanModifyMapByEntrySet();
 		youCantModdifyListCreatedByOfMethod();
 		youCanModdifyListCreatedByAsListMethod();
+	}
+	
+	private static void youCanModifyKeyValuePairOfMapButItHasSomeImportantDetails() {
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("Orange", 10);
+		map.put("Apple", 20);
+		map.put("Melon", 30);
+		System.out.print("Before modify: " + map);
+		try {			
+//			map.keySet().add("Strawberry"); // You can't do that
+//			map.values().add(50); // You can't do that
+			map.keySet().remove("Orange"); // But remove is valid operation - removes [k, v] pair 
+			map.values().remove(20); // Is valid. It removes [k, v] pair
+			System.out.println(" After modify: " + map);
+		} catch(UnsupportedOperationException e) {
+			System.err.println("I can't modify map's keyset!");
+		}
 	}
 	
 	private static void youCantSortMapByEntrySet() {
