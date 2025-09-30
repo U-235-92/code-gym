fe3ef6038b4719c70065ec54e63be31ae6fcce22
U-235-92 <a745296@yandex.ru>
2025-09-30 13:51:53 +0400

    Stream reduce function practise

---
+++ src/main/java/aq/gym/stream/practise/ReduceAndCombineDemo.java
@@ -0,0 +1,42 @@
+package aq.gym.stream.practise;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ReduceAndCombineDemo {
+
+	public static void main(String[] args) {
+		parallelStreamReduceExample();
+		regularStreamReduceExample();
+	}
+
+	private static void parallelStreamReduceExample() {
+		List<Integer> parallelStreamReduceWorkExample = Stream.iterate(1, n -> n + 1).limit(3).toList(); 
+		int parallelStreamResult = parallelStreamReduceWorkExample.parallelStream().reduce(5, 
+				(a, b) -> {
+					System.out.println(String.format("Accumulator call: %d + %d", a, b));
+					return a + b;
+		}, 
+				(a, b) -> {
+					System.out.println(String.format("Combiner call: %d * %d", a, b));
+					return a * b;
+		});
+		System.out.println("Parallel stream reduce work example:");
+		System.out.println(parallelStreamResult);
+	}
+	
+	private static void regularStreamReduceExample() {
+		List<Integer> regularStreamReduceWorkExample = Stream.iterate(1, n -> n + 1).limit(3).toList(); 
+		int regularStreamResult = regularStreamReduceWorkExample.stream().reduce(5, 
+				(a, b) -> {
+					System.out.println(String.format("Accumulator call: %d + %d", a, b));
+					return a + b;
+		}, 
+				(a, b) -> {
+					System.out.println(String.format("Combiner call: %d * %d", a, b));
+					return a * b;
+		});
+		System.out.println("Regular stream reduce work example:");
+		System.out.println(regularStreamResult);
+	}
+}
