9c2dfcbf3c1303140c30abb787d0957fc33cbf27
U-235-92 <a745296@yandex.ru>
2025-10-15 18:06:27 +0400

    Anonymous class no declared method using

--- src/main/java/aq/gym/work_with_objects/inheritence/ABCTestDrive.java
+++ src/main/java/aq/gym/work_with_objects/inheritence/ABCTestDrive.java
@@ -7,5 +7,18 @@ public class ABCTestDrive {
 		A polyA = new C();
 		System.out.println("+++++++++ Create A = new A() +++");
 		A a = new A();
+		B b = new B() {
+			
+			@Override
+			public void abstractFromB() {
+				thisMethodDoesntDefineInB(); // But you're able to use that method inside declared methods of class; 
+			}
+			
+			public void thisMethodDoesntDefineInB() {
+				System.out.println("Trick it!");
+			}
+		};
+//		b.thisMethodDoesntDefineInB(); // ERR! You can't use defined method in Anonymous class;
+		b.abstractFromB();
 	}
 }
