b3c9499935d6a4a4a4e39c85c11ce6fd4d0d1bea
U-235-92 <a745296@yandex.ru>
2025-10-17 12:23:49 +0400

    Leaf similar trees

---
+++ src/main/java/aq/gym/contests/tree/LeafSimilarTrees.java
@@ -0,0 +1,32 @@
+package aq.gym.contests.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeafSimilarTrees {
+
+//	https://leetcode.com/problems/leaf-similar-trees/description
+	public static void main(String[] args) {
+		
+	}
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+    	List<Integer> leafes1 = new ArrayList<Integer>();
+    	List<Integer> leafes2 = new ArrayList<Integer>();
+        accumulateLeafes(root1, leafes1);
+        accumulateLeafes(root2, leafes2);
+        return leafes1.equals(leafes2);
+    }
+    
+    private void accumulateLeafes(TreeNode node, List<Integer> accumulator) {
+    	if(node == null) {
+    		return;
+    	}
+    	if(node.left == null && node.right == null) {
+    		accumulator.add(node.val);
+    		return;
+    	}
+    	accumulateLeafes(node.left, accumulator);
+    	accumulateLeafes(node.right, accumulator);
+    }
+}
