a1e643bd92d8ca43a94cad4f90bf496edb8a8fa3
U-235-92 <a745296@yandex.ru>
2025-11-07 21:41:25 +0400

    Permutations

--- src/main/java/aq/gym/contests/backtracking/Permutations.java
+++ src/main/java/aq/gym/contests/backtracking/Permutations.java
@@ -10,6 +10,8 @@ public class Permutations {
 	public static void main(String[] args) {
 		int[] nums = {1,2,3};
 		System.out.println(new Permutations().permute(nums));
+		String line = "ABC";
+		new Permutations().permute(line, 0, line.length() - 1);
 	}
 	
 	public List<List<Integer>> permute(int[] nums) {
@@ -36,5 +38,26 @@ public class Permutations {
             permutation(permutations, temp, elementsToPermute);
         }
         elementsToPermute.add(curr);
+    }
+    
+    private void permute(String str, int l, int r) {
+        if (l == r) {
+            System.out.println(str);
+        } else {
+            for (int i = l; i <= r; i++) {
+                str = swap(str, l, i);
+                permute(str, l + 1, r);
+                str = swap(str, l, i);
+            }
+        }
+    }
+ 
+    public String swap(String a, int i, int j) {
+        char temp;
+        char[] charArray = a.toCharArray();
+        temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return String.valueOf(charArray);
     }
 }
