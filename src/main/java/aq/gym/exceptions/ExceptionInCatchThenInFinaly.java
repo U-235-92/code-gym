9b2bf194afb69aed3e2fa77e93076d0e182c0f5f
U-235-92 <a745296@yandex.ru>
2025-11-16 18:35:02 +0400

    Added addition example of exception

--- src/main/java/aq/gym/exceptions/ExceptionInCatchThenInFinaly.java
+++ src/main/java/aq/gym/exceptions/ExceptionInCatchThenInFinaly.java
@@ -1,10 +1,19 @@
 package aq.gym.exceptions;
 
+import java.io.IOException;
+
 public class ExceptionInCatchThenInFinaly {
 
 	public static void main(String[] args) {
-		int num = getNumber("forty_two");
+		int num = getNumber("42");
 		System.out.println(num);
+		System.out.println("Before exception");
+		try {
+			throwCheckUncheckManual();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("After exception");
 	}
 
 	private static int getNumber(String number) {
@@ -19,5 +28,14 @@ public class ExceptionInCatchThenInFinaly {
 			
 		}
 		return num; // If uncomment code above this line will be unreachable and this code won't compile
+	}
+	
+	protected static void throwCheckUncheckManual() throws IOException {
+		throw new IOException(); // You have to add throws declaration if you throw checked exception
+//		throw new RuntimeException(); // You don't have to add throws declaration if you throw unchecked exception
+	}
+	
+	protected static void doUnchecked() {
+		throw new RuntimeException();
 	}
 }
