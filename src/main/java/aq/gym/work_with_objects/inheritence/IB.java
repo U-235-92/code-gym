280a5b50e20a9209da22dfff0a0841698fd92cb3
U-235-92 <a745296@yandex.ru>
2025-11-18 15:20:08 +0400

    Added functional interface features

--- src/main/java/aq/gym/work_with_objects/inheritence/IB.java
+++ src/main/java/aq/gym/work_with_objects/inheritence/IB.java
@@ -1,6 +1,21 @@
 package aq.gym.work_with_objects.inheritence;
 
+@FunctionalInterface
 public interface IB {
 
-	void hello();
+	void hello(); 
+//	void bye(); // ERR! You can define ONLY one abstract method in functional interface 
+	
+	static void test() {
+		System.out.println("You can define a static method in a functional interface");
+	}
+	
+	public default void testDefault() {
+		testPrivate();
+		System.out.println("You can define a default method in a functional interface");
+	}
+	
+	private void testPrivate() {
+		System.out.println("You can define a private method in a functional interface");
+	}
 }
