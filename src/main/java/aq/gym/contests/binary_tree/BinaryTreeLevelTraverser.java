aedaac732f6e8c17bb3f5c111797e3d5c57a9554
U-235-92 <a745296@yandex.ru>
2025-11-24 11:41:00 +0400

    Util binary tree traverser

---
+++ src/main/java/aq/gym/contests/binary_tree/BinaryTreeLevelTraverser.java
@@ -0,0 +1,33 @@
+package aq.gym.contests.binary_tree;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public final class BinaryTreeLevelTraverser {
+
+	protected static void traverse(TreeNode root) {
+		List<List<TreeNode>> levelOrderTraverse = new LinkedList<>();
+		Queue<TreeNode> nextLevelNodes = new ArrayDeque<>();
+		Queue<TreeNode> currLevelNodes = new ArrayDeque<>();
+		currLevelNodes.offer(root);
+		levelOrderTraverse.add(List.of(root));
+		while(!currLevelNodes.isEmpty()) {
+			TreeNode node = currLevelNodes.poll();
+			if(node.right != null) {
+				nextLevelNodes.add(node.right);
+			}
+			if(node.left != null) {
+				nextLevelNodes.add(node.left);
+			}
+			if(currLevelNodes.isEmpty()) {
+				currLevelNodes = nextLevelNodes;
+				nextLevelNodes = new ArrayDeque<>();
+				levelOrderTraverse.add(new ArrayList<>(currLevelNodes));
+			}
+		}
+		levelOrderTraverse.forEach(System.out::println);
+	}
+}
